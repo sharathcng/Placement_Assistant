c4569ed0c7b49c92eaefb8396cc3d7763c35254c
unknown <cngsharath@gmail.com>
2020-10-24 14:08:11 +0530

    academic table created

--- Student/admin.py
+++ Student/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student_Profile
+from .models import *
 
 # Register your models here.
 @admin.register(Student_Profile)
@@ -11,3 +11,6 @@ class Student_Profile_Admin(admin.ModelAdmin):
     search_fields = ('username','hobbies')
     date_hierarchy = 'dateOfBirth'
     ordering = ('username','dateOfBirth')
+admin.site.register(Academic_table)
+admin.site.register(skills)
+admin.site.register(projects)
