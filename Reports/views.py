bdfdc54b850b21b1740cc361d66876677effef5c
unknown <cngsharath@gmail.com>
2020-12-10 10:30:43 +0530

    final ppt

--- Reports/views.py
+++ Reports/views.py
@@ -9,46 +9,46 @@ from django.db.models import Sum
 
 
 
-# def report(request):  # Drive Stundent Html Page.
-#     year = Company.objects.values('Year').distinct()
-#     #print(year)
-#     # a = year[0]
-#     # b = year[1]
-#     #print(a['Year'],b['Year'])
-#     years = []
-#     # for y in year :
-#     #     i = y['Year']
-#     #     years.append(i)
-#     #     years.append(Company.objects.filter(Year=y['Year']).count())
-#     # print(years)
-#     for y in year :
-#         #print(y['Year'])
-#         z = str(y['Year'])
-#         years.append([z,Company.objects.filter(Year=y['Year']).count()])
-#     #print(years)
+def report(request):  # Drive Stundent Html Page.
+    year = Company.objects.values('Year').distinct()
+    #print(year)
+    # a = year[0]
+    # b = year[1]
+    #print(a['Year'],b['Year'])
+    years = []
+    # for y in year :
+    #     i = y['Year']
+    #     years.append(i)
+    #     years.append(Company.objects.filter(Year=y['Year']).count())
+    # print(years)
+    for y in year :
+        #print(y['Year'])
+        z = str(y['Year'])
+        years.append([z,Company.objects.filter(Year=y['Year']).count()])
+    #print(years)
 
-#     # l = []
-#     # for j in years:
-#     #     print(years.keys())
-#     #     for b in range(len(years)):
-#     #         l[b]+=years.keys[b]
-#     #         l[b]+=years.values[b]
-#     # print(l)
-#     return render(request, "Reports/reportHome.html",{'years':years})
+    # l = []
+    # for j in years:
+    #     print(years.keys())
+    #     for b in range(len(years)):
+    #         l[b]+=years.keys[b]
+    #         l[b]+=years.values[b]
+    # print(l)
+    return render(request, "Reports/reportHome.html",{'years':years})
 
 
-def bookir_chart(request):
-    labels = []
-    data = []
-    qury = Company.objects.values('Year').distinct()
-    dept2 = Company.objects.values(
-        'Company_Name').annotate(Sum('Company_Name'))
-    for entry in dept2:
-        data.append(entry['Company_Name'])
-    for val in qury:
-      labels.append(val['Year'])
-    data = {
-        'labels': labels,
-        'data': data
-    }
-    return JsonResponse(data)
+# def bookir_chart(request):
+#     labels = []
+#     data = []
+#     qury = Company.objects.values('Year').distinct()
+#     dept2 = Company.objects.values(
+#         'Company_Name').annotate(Sum('Company_Name'))
+#     for entry in dept2:
+#         data.append(entry['Company_Name'])
+#     for val in qury:
+#       labels.append(val['Year'])
+#     data = {
+#         'labels': labels,
+#         'data': data
+#     }
+#     return JsonResponse(data)
